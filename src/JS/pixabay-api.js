88296ef47a97dbf04a011739fe0a472e5b09c0d0
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-02-24 10:14:19 +0200

    create form html

---
+++ src/JS/pixabay-api.js
@@ -0,0 +1,27 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+// import axios from 'axios'; ще потрібен npm install axios
+const ENDPOINT = 'https://pixabay.com/api/';
+const KEY = '33796051-1e422d07d8c9fc912bc2a7eb4';
+const param =
+  'per_page=40&image_type=photo&orientation=horyzontal&safesearch=true';
+
+export default class ImageApiService {
+  constructor() {
+    this.page = 1;
+    this.search = '';
+  }
+  async getImage() {
+    const URL = `${ENDPOINT}?key=${KEY}&q=${this.search}&page=${this.page}&${param}`;
+    const response = await axios.get(URL);
+    this.nextPage();
+
+    return response.data;
+  }
+  nextPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
